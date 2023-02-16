b951cd5394d97e9617c297a04d389e30594722e0
SalimSOUFIT <salime.soufite@gmail.com>
2023-02-16 11:14:56 +0100

    Version 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './product/product-list/product-list.component';
+
 
 const routes: Routes = [
-  {path: 'products', component : ProductListComponent}
+  {
+    path: '', loadChildren: () => import('./public/public.module')
+      .then(m => m.PublicModule)
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './shared/footer/footer.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent
+
   ],
   imports: [
     BrowserModule,
