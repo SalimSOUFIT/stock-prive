9417f27d780c71675a445bec0c124a3332396c3c
SalimSOUFIT <salime.soufite@gmail.com>
2023-03-15 13:37:12 +0100

    update version

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -32,8 +32,10 @@ export class HomeComponent implements OnInit{
 
   config: SwiperOptions = {
     slidesPerView: 3,
-    spaceBetween: 10,
+    spaceBetween: 100,
     navigation: true,
-    slideClass: 'my-slide'
+    // slideClass: 'my-slide'
+
   };
+
 }
